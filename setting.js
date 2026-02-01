1efee477509130229e0f5bb8750017397265c173
Malvin King <malvintech2@gmail.com>
2026-02-01 23:49:25 +0200

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,10 @@
+//===== Obsidian md====
+
+module.exports = {
+  BOT_NAME: 'ᴏʙsɪᴅɪᴀɴ ᴍᴅ', // ur bot name
+  OWNER_NAME: 'ᴛʜᴇ sᴛɪᴀɴ', //your name
+  OWNER_NUMBER: '', // add your phone number
+  SESSION_ID: '', // use sessin id if inbuilt pairing not working
+  MODE: "public", // public or private
+  TIMEZONE: 'Africa/Harare', // ur timezone
+};
